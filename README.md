e13598a15ecaa5d4f5729fe33da8de656a54aa7d
oglass <alexbirdand@gmail.com>
2024-07-03 23:45:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Blade
+
+A batteries-included and modular Minestom Network framework.
+
+---
+
+### Adding as a dependency
+
+```kotlin
+repositories {
+    maven("https://mvn.bladehunt.net/releases")
+}
+
+dependencies {
+    // kotlinx.coroutines is required
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+    implementation("net.bladehunt:blade:0.1.0-alpha.1")
+}
+```
+
+---
+
+### Example
+
+Visit [the example folder](/example) to view an example.
+
+- Clone the repository: `git clone https://github.com/bladehuntmc/MinigameLib`
+- Run the example in IntelliJ
+- Join on two Minecraft instances
+- Enter the command `/join` on both. (Note: The instance takes time to load, so if entered too quickly, the game will
+  start before fully loaded)
---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "2.0.0"
+    id("io.github.goooler.shadow") version "8.1.7"
+
+    `maven-publish`
+
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+allprojects {
+    group = property("group") ?: "net.bladehunt"
+    version = property("version") ?: "0.1.0"
+
+    repositories {
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://mvn.bladehunt.net/releases")
+    }
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
+    }
+
+    tasks.withType<KotlinCompile> {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+            compilerOptions { freeCompilerArgs.addAll("-Xinline-classes", "-Xcontext-receivers") }
+        }
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
+}
+
+dependencies {
+    compileOnly("net.minestom", "minestom-snapshots", property("minestom.version") as String)
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+
+    api("net.bladehunt:kotstom:0.3.0-alpha.1")
+    api("net.bladehunt:kotstom-adventure-serialization:0.3.0-alpha.1")
+    api("io.github.cdimascio:dotenv-kotlin:6.4.1")
+
+    api("io.klogging:slf4j-klogging:0.5.14")
+
+    testImplementation("net.minestom", "minestom-snapshots", property("minestom.version") as String)
+    testImplementation("io.kotest:kotest-assertions-core:5.9.0")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.0")
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("blade")
+    mergeServiceFiles()
+    minimize()
+}
+
+tasks.withType<Test> { useJUnitPlatform() }
+
+tasks.build { dependsOn("shadowJar") }
+
+java { withSourcesJar() }
+
+publishing {
+    publications {
+        create<MavenPublication>("library") {
+            from(components["java"])
+            artifactId = "blade"
+        }
+    }
+    repositories {
+        maven {
+            name = "releases"
+            url = uri("https://mvn.bladehunt.net/releases")
+            credentials(PasswordCredentials::class) {
+                username = System.getenv("MAVEN_NAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+            authentication { create<BasicAuthentication>("basic") }
+        }
+    }
+}
